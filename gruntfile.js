7eb0f4d713bb1281bbd76af2ebbd6fcaba3ed8e8
hugohoover <hugohoover.denovo@gmail.com>
2016-09-22 00:40:56 +0800

    updated jekyll sass and gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -1,11 +1,11 @@
-/// 包装函数
+// 包装函数 // Do grunt-related things in here
 module.exports = function(grunt) {
 
 require('load-grunt-tasks')(grunt);
 // 任务配置,所有插件的配置信息
 grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-//    當使用stylus的時候去注釋
+//  當使用stylus的時候去注釋
 //    stylus:{
 //        build: {
 //            options: {
@@ -50,7 +50,7 @@ grunt.initConfig({
     });
     // 告诉grunt我们将使用插件
     grunt.loadNpmTasks('grunt-contrib-watch');
-//    grunt.loadNpmTasks('grunt-contrib-stylus');
+    // grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-contrib-sass');
     // 告诉grunt当我们在终端中输入grunt时需要做些什么
     grunt.registerTask('default', ['watch'], ['sass']);
